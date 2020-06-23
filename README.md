aedc64cfd4456dffd26e3090e8551fb33881141a
Anjini Krishnan <anjini.krishnan@hotmail.com>
2020-06-23 15:23:54 +0930

    basic layout complete and function working need weather icons and saving local info to be added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# WeatherDashboard
+# Weather Dashboard
+
+## Credits
+https://getbootstrap.com/docs/4.5/getting-started/introduction/
+https://openweathermap.org/api/one-call-api
---
+++ script.js
@@ -0,0 +1,92 @@
+//this is my APIKEY
+var APIkey = "2d651d973073a30ce5cf8e74013d0663";
+
+//on click event to determine location
+$(".buttons").on("click", function () {
+  var city = this.value;
+  var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL1,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+      console.log(response);
+      //necessary parameters
+      var lattitude = response.coord.lat;
+      var longittude = response.coord.lon;
+      //building url to query database
+      var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longittude + "&appid=" + APIkey;
+
+      //run AJAX call to the OpenWeatherMap API
+      $.ajax({
+        url: queryURL2,
+        method: "GET"
+      })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (response2) {
+
+          // Log the queryURL
+          console.log(queryURL2);
+
+          // Log the resulting object
+          console.log(response2);
+
+          // Transfer content to HTML
+          $(".city").html("<h1>" + city + " Weather Details</h1>");
+          $(".wind").text("Wind Speed: " + response2.current.wind_speed);
+          $(".humidity").text("Humidity: " + response2.current.humidity);
+
+          // Convert the temp to fahrenheit
+          var tempF = (response2.current.temp - 273.15) * 1.80 + 32;
+
+          // add temp content to html
+          $(".temp").text("Temperature (K): " + response2.current.temp);
+          $(".tempF").text("Temperature (F): " + tempF.toFixed(2));
+          
+          //creating 5day forecast
+          for (var i = 0; i < 6; i++) {
+            //current date
+          var d = new Date();
+          //returns day as number 1 to 31
+          var date = d.getDate();
+          //returns month as number 0 to 11
+          var monthIndex = d.getMonth();
+          //returns year
+          var year = d.getFullYear();
+            //main card div
+            var heading = $("#"+i);
+            heading.text((date+i+1) + "/" + (monthIndex + 1) + "/" + year);
+            //weather icon object
+            var icon=$("#icon"+i);
+            var weatherIcon ={
+                "Rain":"fas fa-cloud-rain",
+                "Thunderstorm":"fas fa-bolt",
+                "Clouds":"fas fa-cloud"
+            }
+            var weatherType=response2.daily[i].weather[0].main;
+            icon.attr("class",weatherIcon.weatherType);
+            console.log(weatherIcon.weatherType);
+            //variables to points in HTML
+            var temp_i =$(".temp"+i);
+            var tempF_i=$(".tempF"+i);
+            var humidity_i=$(".humidity"+i);
+            //card words
+            var dayTK =response2.daily[i].temp.day;
+            var dayTF =((response2.daily[i].temp.day - 273.15) * 1.80 + 32).toFixed(2);
+            var dayHumidity=response2.daily[i].humidity;
+            //combined
+            temp_i.text("Temperature (K): "+dayTK);
+            tempF_i.text("Temperature (F): "+dayTF);
+            humidity_i.text("Humidity: "+dayHumidity);
+
+          }
+
+        });
+
+    });
+
+});
+
